50ca9ccc5d82ce1f68bc1cf099507067b69efab2
Emanuel <emanuel.beni@gmail.com>
2020-04-13 13:30:35 +0700

    Fixed typo error for key name

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 // Production Keys only
 module.exports = {
-	googleClientId: process.env.GOOGLE_CLIENT_ID,
+	googleClientId: process.env.GOOGLE_CLIENT_SECRET,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	cookieKey: process.env.COOKIE_KEY,
 	mongoURI: process.env.MONGO_URI,
