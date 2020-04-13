6340d04a0199e69faef72197264c923a24b6209e
Emanuel <emanuel.beni@gmail.com>
2020-04-13 13:08:44 +0700

    Unecessary comments removed

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,3 @@ module.exports = {
 	cookieKey: process.env.COOKIE_KEY,
 	mongoURI: process.env.MONGO_URI,
 };
-
-// mongodb+srv://Emanuel:0wMq263WWE6WaG58@cluster0-9hxu7.mongodb.net/test?retryWrites=true&w=majority
