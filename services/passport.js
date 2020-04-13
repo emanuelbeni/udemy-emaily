88b6d265825f812938eaa32000a19c0754eca5f8
Emanuel <emanuel.beni@gmail.com>
2020-04-13 14:20:33 +0700

    Set proxy to true to have SSL upon callback

--- services/passport.js
+++ services/passport.js
@@ -12,6 +12,7 @@ passport.use(
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
 			callbackURL: "/auth/google/callback",
+			proxy: true,
 		},
 		(accessToken, refreshToken, profile, done) => {
 			// Check if user exists in our DB
