19d046f23dc7db73e13122493492015c60a779f4
Emanuel <emanuel.beni@gmail.com>
2020-04-13 13:32:50 +0700

    Fix error

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 // Production Keys only
 module.exports = {
-	googleClientId: process.env.GOOGLE_CLIENT_SECRET,
+	googleClientId: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	cookieKey: process.env.COOKIE_KEY,
 	mongoURI: process.env.MONGO_URI,
